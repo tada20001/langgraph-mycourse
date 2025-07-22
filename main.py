fa7c3831390cc7a9936689cae433833bf9dc9e1c
tada20001 <tada20001@gmail.com>
2025-07-22 09:50:52 +0900

    function calling reasoning

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from dotenv import load_dotenv
+from langgraph.graph import StateGraph, START, END
+from langgraph.prebuilt import create_react_agent
+from langchain_openai import ChatOpenAI
+from langchain_core.tools import tool
+from langchain_tavily import TavilySearch
 
 
 load_dotenv()
 
-if __name__ == "__main__":
-    print("Hello ReAct LangGraph with Function Calling")
+llm = ChatOpenAI(model="gpt-4o-mini", temperature=0)
